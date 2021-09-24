9a1d3554658ea34cf0cb8f124226293c65db871c
vaibhav117 <vaibhavmathur117@gmail.com>
2021-09-24 03:30:45 -0400

    Linear decay

--- slurm_train.sh
+++ slurm_train.sh
@@ -5,12 +5,12 @@
 #SBATCH --cpus-per-task=8
 #SBATCH --time=05:00:00
 #SBATCH --mem=100GB
-#SBATCH --job-name=hw1_expert_explore
+#SBATCH --job-name=hw1_dagger_explore_linear_decay
 #SBATCH --mail-type=END
 #SBATCH --gres=gpu
 #SBATCH --array=1
-#SBATCH --output=./logs/expert_explore/%j_%x.out
-#SBATCH --error=./logs/expert_explore/%j_%x.err
+#SBATCH --output=./logs/dagger_explore_linear_decay/%j_%x.out
+#SBATCH --error=./logs/dagger_explore_linear_decay/%j_%x.err
 #SBATCH --export=ALL
 
 bash ./run-mpi4py-singularity.bash \

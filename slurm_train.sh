3dc7c75a5578f2d537adff0ba8078f2d50d9b20b
vaibhav117 <vaibhavmathur117@gmail.com>
2021-09-24 07:11:42 -0400

    high res

--- slurm_train.sh
+++ slurm_train.sh
@@ -5,12 +5,12 @@
 #SBATCH --cpus-per-task=8
 #SBATCH --time=05:00:00
 #SBATCH --mem=100GB
-#SBATCH --job-name=hw1_dagger_explore_exponential_decay
+#SBATCH --job-name=hw1_dagger_explore_exponential_decay_highres
 #SBATCH --mail-type=END
 #SBATCH --gres=gpu
 #SBATCH --array=1
-#SBATCH --output=./logs/dagger_explore_exponential_decay/%j_%x.out
-#SBATCH --error=./logs/dagger_explore_exponential_decay/%j_%x.err
+#SBATCH --output=./logs/dagger_explore_exponential_decay_highres/%j_%x.out
+#SBATCH --error=./logs/dagger_explore_exponential_decay_highres/%j_%x.err
 #SBATCH --export=ALL
 
 bash ./run-mpi4py-singularity.bash \

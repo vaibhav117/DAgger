89365776fd2903dc3c45b6837a00afeb6bc7438a
vaibhav117 <vaibhavmathur117@gmail.com>
2021-09-24 06:50:59 -0400

    TRial

--- trial.py
+++ trial.py
@@ -13,6 +13,7 @@ import numpy as np
 
 from reacher_env import ReacherDaggerEnv
 from utils import weight_init, ExpertBuffer
+import matplotlib.pyplot as plt
 
 
 env = ReacherDaggerEnv()
@@ -20,8 +21,27 @@ env.reset()
 
 print(f"action_space: {env.action_space.shape[0]}")
 
-for _ in range(1000):
-    action = env.get_expert_action()
-    visual_obs, reward, done, info = env.step(action)
-    print(f"shape of visual_obs:{visual_obs.shape}\n")
-    print(f"reward:{reward} , action:{action}\n")
+def try2():
+    for _ in range(1000):
+        action = env.get_expert_action()
+        visual_obs, reward, done, info = env.step(action)
+        visual_obs = np.moveaxis(visual_obs, 0, -1)
+        plt.imshow(visual_obs)
+        plt.show()
+        
+        print(f"shape of visual_obs:{visual_obs.shape}\n")
+        print(f"reward:{reward} , action:{action}\n")
+
+
+
+def try1():
+    env.reset()
+    for i in range(100):
+        env.step(env.action_space.sample())
+        rgb_array_large = env.render(
+            mode="rgb_array",
+        )
+        plt.imshow(rgb_array_large)
+        plt.show()
+
+try1()
